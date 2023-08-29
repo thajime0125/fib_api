8340f3a7d76ef47e894c6374e171d7f362e69461
thajime0125 <thajime0125@gmail.com>
2023-08-29 10:12:57 +0900

    フィボナッチ数を返すAPIの実装

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        an_1 = 1
+        an_2 = 1
+        for i in range(2, n):
+            fib = an_1 + an_2
+            an_1 = an_2
+            an_2 = fib
+        return fib
+
+def bad_request():
+    content = {"status": 400, "massage": "Number must be greater than 0"}
+    return JSONResponse(status_code=400, content=content)
+
+@app.get("/fib")
+def get_fibonacci(n: int = Query(...)):
+    if n < 1:
+        return bad_request()
+    return {"result": fibonacci(n)}
